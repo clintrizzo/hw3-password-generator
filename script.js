739b6582ba6ac06826fb794511e68fc9e5780ac0
Clint <maxeva02@gmail.com>
2020-12-20 15:10:40 -0800

    alert is now active for when the length is not met

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//variables used for the code
 var password = document.getElementById("passwordBox");
 var minLength = 8;
 var maxLenght = 128;
@@ -6,6 +7,7 @@ var numberValue = document.getElementById("numberValue");
 var lowerCase = document.getElementById("lowerCase");
 var upperCase = document.getElementById("upperCase");
 
+//functions to have the if statements met
 function createPassword() {
     let lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
     let upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -13,24 +15,27 @@ function createPassword() {
     let specialCharacters = "!@#$%^&*)(+=]{[}?/";
     let passwordLength = prompt('Select the length of your password. Password needs to be at least 8 to 128 characters long')
 
-    lowerCase = document.getElementById("lowerCase").checked;
+    lowerCase = document.getElementById("lowerCase").checked; //for user to check the boxes they want for their password
     upperCase = document.getElementById("upperCase").checked;
     specialChar = document.getElementById("specialChar").checked;
     numberVal = document.getElementById("numberVal").checked;
     passwordBox = document.getElementById("passwordbox");
 
-    if (passwordLength < minLength && passwordLength > maxLenght) {
-        alert('You need to select a value between 8 and 128')
-        return false;
+    //if user does not select a length between 8 and 128 an alert will appear for them to choose a new value
+    if (passwordLength != minLength && passwordLength != maxLenght) {
+        alert("Please try again. Select a length between 8 and 128")
     }
 
+    //if condition to be met, alert to be called if condition is not met
     if (lowerCase == false && upperCase == false && numberVal == false && specialChar == false) {
-        alert('You need to select at least one of the boxes below to receive your password.')
+        alert('Please try again. You need to check one of the boxes below to receive your password.')
     }
 
+    //password will run if the length is met
     if (passwordLength >= minLength && passwordLength <= maxLenght) {
         password = "";
 
+        //the if statements to pull from the functions
         for (i = 0; i < passwordLength; i++) {
             if (lowerCase && password.length < passwordLength) {
                 password += lowerCaseLetters.charAt(Math.floor(Math.random() * lowerCaseLetters.length));
@@ -45,6 +50,8 @@ function createPassword() {
                 password += numbers.charAt(Math.floor(Math.random() * numbers.length));
             }
         }
+
+        //text to appear in the box of the password created
         passwordBox.value = password
     }
 }
