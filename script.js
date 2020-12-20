439caedf59a58d6a75702c1c8c5cc5801a52d3f5
Clint <maxeva02@gmail.com>
2020-12-20 13:43:35 -0800

    debugged the generate password button.

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
 var password = document.getElementById("passwordBox");
 var minLength = 8;
 var maxLenght = 128;
-var specialCharacters = document.getElementById("specialCharacter")[0];
-var numberValue = document.getElementById("numberValue")[0];
-var lowerCase = document.getElementById("lowerCase")[0];
-var upperCase = document.getElementById("upperCase")[0];
+var specialCharacters = document.getElementById("specialCharacter");
+var numberValue = document.getElementById("numberValue");
+var lowerCase = document.getElementById("lowerCase");
+var upperCase = document.getElementById("upperCase");
 
 function createPassword() {
     let lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
     let upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let numbers = "1234567890";
-    let specialCharacters = "!@#$%^&*)(+=]{[}?/"
+    let specialCharacters = "!@#$%^&*)(+=]{[}?/";
 
-    lowerCase = document.getElementById("lowerCase")[0].checked;
-    upperCase = document.getElementById("upperCase")[0].checked;
-    specialChar = document.getElementById("specialCharacter")[0].checked;
-    numberVal = document.getElementById("numberValue")[0].checked;
 
-    let passwordLength = prompt('Select the length of your password.')
+
+    lowerCase = document.getElementById("lowerCase");
+    upperCase = document.getElementById("upperCase");
+    specialChar = document.getElementById("specialChar");
+    numberVal = document.getElementById("numberVal");
+    passwordBox = document.getElementById("passwordbox")
+
+    let passwordLength = prompt('Select the length of your password. Password needs to be at least 8 to 128 characters long')
+
 
     if (lowerCase == false && upperCase == false && specialChar == false && numberVal == false) {
         alert('You need to select at least one of the boxes below to receive your password.')
     }
-}
+
+    if ((passwordLength < minLength && passwordLength > maxLenght) {
+            alert('choose a valid length')
+        }
+
+        if (passwordLength >= minLength && passwordLength <= maxLenght) {
+            password = "";
+
+            console.log(lowerCase, upperCase, specialChar, numberVal)
+
+            for (i = 0; i < passwordLength; i++) {
+                if (lowerCase && password.length < passwordLength) {
+                    password += lowerCaseLetters.charAt(Math.floor(Math.random() * lowerCaseLetters.length));
+                }
+                if (upperCase && password.length < passwordLength) {
+                    password += upperCaseLetters.charAt(Math.floor(Math.random() * upperCaseLetters.length));
+                }
+                if (specialChar && password.length < passwordLength) {
+                    password += specialCharacters.charAt(Math.floor(Math.random() * specialCharacters.length));
+                }
+                if (numberVal && password.length < passwordLength) {
+                    password += numbers.charAt(Math.floor(Math.random() * numbers.length));
+                }
+            }
+            // radomize password
+            passwordBox.value = password;
+        }
+    }
